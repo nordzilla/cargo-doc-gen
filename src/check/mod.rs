804cb5adb173c793a325a8edd5f43217af06769c
ObliqueMotion <ObliqueMotionCode@gmail.com>
2021-04-10 17:18:56 -0700

    Add modules that document the data pipeline and workflow
    All naming conventions, types, and descriptions are heavily
    subject to change. The intent of this commit is to provide a
    concrete structure for a starting point to begin exploring the
    different components that will be required for this project.

---
+++ src/check/mod.rs
@@ -0,0 +1,14 @@
+//! The main entry point for the `doc-gen --check` command option.
+//! Handles validating that existing documentation in a rust project
+//! conforms to the specifications set by `DocConfig`.
+//!
+//! Should also be capable of determining if documtnation is out of date
+//! or incomplete.
+//!
+//! An example of documentation being out of date would be if `DocConfig`
+//! has specified that the arguments to functions should be enumerated
+//! with descriptions, but the function arguments and/or arg names have
+//! changed since it was last documented.
+//!
+//! Examples of documentation being incomplete would be an empty examples
+//! section, or unfilled descriptions for enumerated function arguments.
---
+++ src/doc_gen/mod.rs
@@ -0,0 +1,8 @@
+//! Handles generating documentation comments from a `DocTree`.
+
+/// The `DocGenerator` is a type that consumes `DocTree` nodes
+/// and produces a flatened list of formatted documentation comments,
+/// along with the files and line numbers into which the comments should
+/// be inserted. Comments are generated in a style determined by `DocConfig`.
+#[allow(unused)]
+pub struct DocGenerator;
